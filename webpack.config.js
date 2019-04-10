c992ba3bfd0e5a7631a4abdbe861574b91ba9c2a
Kamil Nawrot <kamilnawrot98@gmail.com>
2019-04-10 21:21:06 +0200

    updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
-  entry: "./src/app.js",
-  output: {
-    filename: "./dist/main.js"
-  },
-  watch: true,
-  module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader']
-    }
-  ]
-}
-}
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader" // translates CSS into CommonJS
+                    //"sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader?name=[name].[ext]&outputPath=img/'
+                ]
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
